90a352e20c7a5cda8711fce2c1aeb3bf3b50c691
Neves António <90612989+nevesantonio-pilale@users.noreply.github.com>
2022-02-11 18:02:48 +0200

    trabalhando na responsividade

---
+++ file.js
@@ -0,0 +1,48 @@
+
+function abrirMenu() {
+	
+	document.getElementById('menu').style.display = "block";
+	document.getElementById('img-menu').style.display = "none";
+	
+	document.getElementById('img-close').style.display = "block";
+}
+
+
+function closerMenu() {
+	
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('img-menu').style.display = "block";
+	
+	document.getElementById('img-close').style.display = "none";
+}
+
+
+function fecharMenu() {
+	
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('img-close').style.display = "none";
+	document.getElementById('img-menu').style.display = "block";
+}
+
+function fecharMenu2(){
+	
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('img-close').style.display = "none";
+	document.getElementById('img-menu').style.display = "block";
+} 
+
+
+function fecharMenu3() {
+	
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('img-close').style.display = "none";
+	document.getElementById('img-menu').style.display = "block";
+} 
+
+
+function fecharMenu4() {
+	
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('img-close').style.display = "none";
+	document.getElementById('img-menu').style.display = "block";
+}
